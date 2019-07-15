ef61441b5cf11e57f668b32ec7e191a01aca14f4
Sushant Kumar Shakya <sushant.shakya@wipro.com>
2019-07-15 16:33:53 +0530

    Updates

--- src/components/TimerComponent.js
+++ src/components/TimerComponent.js
@@ -4,17 +4,20 @@ class TimerComponent extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            currentTime:new Date()
+            currentTime:new Date(),
+            bgcolor1:"#27ff00",
+            bgcolor2:"#00d0ff66",
+            bgcolor:"#27ff00"
         }
     }
 
     ticker=()=>{
-        this.setState({currentTime:new Date()});
+        this.setState({currentTime:new Date(),bgcolor:this.state.bgcolor==this.state.bgcolor1?this.state.bgcolor2:this.state.bgcolor1});
     }
 
     render(){
         return(
-            <div>            
+            <div style={{backgroundColor:this.state.bgcolor}}>            
             <h2><span>{this.state.currentTime.getHours()}</span><span>:</span><span>{this.state.currentTime.getMinutes()}</span><span>:</span><span>{this.state.currentTime.getSeconds()}</span></h2>
             </div>
             )
